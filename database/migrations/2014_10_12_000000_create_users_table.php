a7de25bdd324dd9270ef20164fc21297a12e224e
felixyunianto <felixyunianto@gmail.com>
2021-08-07 16:14:23 +0700

    24. Modify add NIk users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,6 +15,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->char('NIK', 16)->nullable();
             $table->string('name', 50);
             $table->string('email', 100)->unique();
             $table->timestamp('email_verified_at')->nullable();
